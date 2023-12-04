75e87fd8a1270da9782f2fa6bb0a688829a1ca07
payal kharat <pkharat@assimilatetechnologies.com>
2023-12-04 18:17:26 +0530

    get token from header

--- src/functions/citizen/handler.ts
+++ src/functions/citizen/handler.ts
@@ -7,22 +7,28 @@ import { createCitizenDetails } from './citizen.service';
 
 const createCitizen = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const userInformation: any = event.headers["user"];
+        if (!userInformation || !userInformation.userId) {
+            return response(401, { message: 'Invalid or missing user information in the token' });
+        }
         // @ts-ignore
         const obj: CitizenCreate = event.body;
-        obj.PK = obj.Domain;
+        obj.PK = userInformation.userId;
         obj.SK = Organization_Sk;
+
         const citizen = await createCitizenDetails(obj);
+
         if (citizen.$metadata.httpStatusCode == 400) {
             console.error('error getting material =>', citizen);
             return response(400, { message: 'ERROR_GETTING_MATERIAL_TYPES' });
         }
+
         return response(200, { message: 'Citizen Added Successfully', item: citizen });
     } catch (error) {
         console.log('error', error);
         return response(500, error);
     }
-})
-
+});
 
 export {
     createCitizen
